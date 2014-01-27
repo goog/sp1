0db01e6d0a36ad77dc0dc61599e73941bc35f280
goog <googcheng@gmail.com>
2014-01-27 14:58:24 +0800

    day9 finished

---
+++ day9/int02.c
@@ -0,0 +1,49 @@
+#include "bootpack.h"
+#include <stdio.h>
+
+void init_pic(void)
+{
+
+io_out8(PIC0_IMR,0xff);  // forbid all interrupt
+io_out8(PIC1_IMR,0xff);
+
+io_out8(PIC0_ICW1,0x11);
+io_out8(PIC0_ICW2,0x20);// receive by INT20-27 
+io_out8(PIC0_ICW3,1 << 2); // 00000100 , the master 2th irq
+io_out8(PIC0_ICW4, 0x01);
+
+io_out8(PIC1_ICW1,0x11);
+io_out8(PIC1_ICW2,0x28);
+io_out8(PIC1_ICW3,2); // pci1 connect to irq2
+io_out8(PIC1_ICW4, 0x01); // no buffer mode 
+
+io_out8(PIC0_IMR,0xfb);
+io_out8(PIC1_IMR,0xff);
+return;
+}
+
+// keyboard handler
+#define PORT_KEYDAT 0x0060
+struct KEYBUF keybuf;
+
+void inthandler21(int * esp)
+{
+	//struct BOOTINFO *binfo = (struct BOOTINFO *)  ADR_BOOTINFO;
+	unsigned char data;
+	io_out8(PIC0_OCW2,0x61);
+	data= io_in8(PORT_KEYDAT);
+	if(keybuf.next < 32) // next is a flag
+	{keybuf.data[keybuf.next] = data; keybuf.next++;}
+	return;
+}
+
+void inthandler2c(int *esp)
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
+	for (;;) {
+		io_hlt();
+		}
+}
+
